d7c94b91043775a5c5081008fb5902c27aa86c35
helenlin305 <lin_helen305@outlook.com>
2022-11-28 14:44:27 -0500

    add dataset

--- similarpair.mli
+++
@@ -1,8 +0,0 @@
-(* A similar pair module can be used to calculate the similarity score *)
-
-type item 
-type t = item * item 
-
-val get_cosine_similarity: t -> float 
-val get_pearson_similarity: t -> float 
-val get_average_similarity: t -> float 
