dfd496777bbde3710fe072ab7292dee9410bfa81
RongYangRosie <112712992+RongYangRosie@users.noreply.github.com>
2022-12-05 14:23:04 -0500

    update movie.mli

--- movie.mli
+++ movie.mli
@@ -1,7 +1,25 @@
-type rating = { 
-  userid : int;
-  movieid : int; 
-  rating : float; 
+type movie =
+  {
+    budget: string;
+    genres: string;
+    homepage: string;
+    id: string;
+    keywords: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: string;
+    production_companies: string;
+    production_countries: string;
+    release_date: string;
+    revenue: string;
+    runtime: string;
+    spoken_languages: string;
+    status: string;
+    tagline: string;
+    title: string;
+    vote_average: string;
+    vote_count: string;
   }
 type result = {
     movie_id: int;
@@ -25,8 +43,8 @@ type credits = credit list
 type movies = movie list
 type t = result list
 
-val parse_credit : string -> credits 
-val parse_movie : string -> movies
-val csv2list: movies -> credits -> t
+val parse_credit : string -> credits
+val parse_movie : string -> movies 
+val csv2list: movies  -> credits -> t
 
 
