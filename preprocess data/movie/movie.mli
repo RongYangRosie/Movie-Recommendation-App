2f42b6d8207ea198ddd056e41e11122985895de4
RongYangRosie <112712992+RongYangRosie@users.noreply.github.com>
2022-12-07 18:05:10 -0500

    update name of movie record

--- preprocess data/movie/movie.mli
+++ preprocess data/movie/movie.mli
@@ -1,53 +1,32 @@
+type rating = { 
+  userid : int;
+  movieid : int; 
+  rating : float; 
+  }
 type result = {
-  movie_id : int;
-  title : string;
-  cast : string list;
-  director : string;
-  keywords : string list;
-  genres : string list;
-  overview : string;
-  popularity : float;
-  vote_count : int;
-  vote_average : float;
+    movie_id: int;
+    title: string;
+    cast: string list;
+    director: string;
+    keywords: string list;
+    genres: string list;
+    overview: string;    
+    popularity: float;
+    vote_count: int;
+    vote_average: float;
 }
-val pp_result : Format.formatter -> result -> unit
-val show_result : result -> string
-type t = result list
-val pp : Format.formatter -> t -> unit
-val show : t -> string
-
-type credit = {
-  movie_id : string;
-  title : string;
-  cast : string;
-  crew : string;
+type credit =  {
+  movie_id: string;
+  title: string;
+  cast: string;
+  crew: string;
 }
 type credits = credit list
-
-val pp_credit : Format.formatter -> credit -> unit
-val show_credit : credit -> string
-val pp_credits : Format.formatter -> credits -> unit
-val show_credits : credits -> string
-
-type movie = {
-  genres : string;
-  id : string;
-  keywords : string;
-  overview : string;
-  popularity : string;
-  title : string;
-  vote_average : string;
-  vote_count : string;
-}
-
 type movies = movie list
-val parse_credit : string -> credit list
-val parse_movies : string -> movie list
-val csv2list : string -> string -> result list
+type t = result list
 
-val pp_movies : Format.formatter -> movies -> unit
-val show_movies : movies -> string
+val parse_credit : string -> credits 
+val parse_movie : string -> movies
+val csv2list: movies -> credits -> t
 
 
-val pp_movie : Format.formatter -> movie -> unit
-val show_movie : movie -> string
